d138af58afb0d45262c836305cfc734835ce11a2
Chadwick Dahlquist <chadwick@bugeats.net>
2015-04-09 15:46:44 -0700

    Script for symlinking

---
+++ symlink.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+FILES=$DIR/dotfiles/*
+
+for FILE_PATH in $FILES; do
+  FILE_NAME=$(basename $FILE_PATH)
+  echo "$FILE_PATH -> ~/.$FILE_NAME"
+  `ln -s -i $FILE_PATH ~/.$FILE_NAME`
+done
+
+echo "DONE"
