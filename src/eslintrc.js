3463e32b9d3242f034e9de0cfaa272878f4a935a
Chadwick Dahlquist <chadwick@bugeats.net>
2015-12-17 10:54:32 -0800

    Add eslintrc and cleanup some crap

---
+++ src/eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    rules: {
+        'camelcase': 1,
+        'comma-dangle': [2, 'never'],
+        'eqeqeq': 2,
+        'no-underscore-dangle': 0,
+        'no-unused-vars': 0,
+        'quotes': [1, single],
+        'semi': [2, always],
+        'strict': 0
+    }
+};
+
